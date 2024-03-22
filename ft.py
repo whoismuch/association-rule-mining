f840753f87166434a2ce5f0cc9c6222942801aa6
Frr-fox <58375442+Frr-fox@users.noreply.github.com>
2024-03-22 19:35:24 +0300

    Use add time and memory usage

--- ft.py
+++ ft.py
@@ -1,5 +1,5 @@
 from collections import defaultdict, namedtuple
-
+from memory_profiler import profile
 
 class FP_Node:
     def __init__(self, tree, item, count=1):
@@ -172,9 +172,10 @@ def mining_frequent(transactions, minSupport):
         yield i
 
 
+@profile
 def fp_growth(transactions, minSupport):
     result = []
     for item, support in mining_frequent(transactions, minSupport):
         result.append((item, support))
-    result.sort(key=lambda x: x[1],reverse=True)
+    result.sort(key=lambda x: x[1], reverse=True)
     return result
